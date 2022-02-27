0f8b08fae14c2f9e08a8193c7cf5b35326f3d582
DanR007 <75842483+DanR007@users.noreply.github.com>
2022-02-27 16:29:49 +0700

    Now RSA algorithm works!

--- MyRandomRSACrypt/MyRandomRSACrypt.cpp
+++ MyRandomRSACrypt/MyRandomRSACrypt.cpp
@@ -6,29 +6,38 @@
 #include <stdlib.h>
 #include <vector>
 #include <time.h>
+#include <filesystem>
+#include <fstream>
 
 //край для поиска простых чисел
 //N >= 13
-#define N 13
+#define N 8000
+#define LL long long int
+#define PR_LL %lld
 
 void MakeArrayWithPrimeNumbersFromElevenToN();
-long int GetRandomPrimeNumber();
-long int GetRandomPrimeInRange(long int left_border, long int right_border);
+LL GetRandomPrimeNumber();
+LL GetRandomPrimeInRange(LL left_border, LL right_border);
 
-void EncryptMessage(std::string message, std::string& encrypt_message, 
-    std::pair<long int, long int> public_key);
-long int GetEncryptNumber(long int number, std::pair<long int, long int> public_key);
+void EncryptMessage(std::string message, std::vector<LL>& encrypt_message_in_dec,
+    std::pair<LL, LL> public_key, std::string& encrypt_message);
+LL GetEncryptNumber(LL number, std::pair<LL, LL> public_key);
 
-void DecryptMessage(std::string encrypt_message, std::string& decrypt_message,
-    std::pair<long int, long int> private_key);
-long int GetDecryptNumber(long int number, std::pair<long int, long int> private_key);
+void DecryptMessage(std::vector<LL>& encrypt_message_in_dec, std::string& decrypt_message,
+std::pair<LL, LL> private_key);
+LL GetDecryptNumber(LL number, std::pair<LL, LL> private_key);
 
 
-long int GetFirstPartPublicKey(long int fi);
-long int GetFirstPartPrivateKey(long int fi, long int first_part_public_key);
+LL GetFirstPartPublicKey(LL fi);
+LL GetFirstPartPrivateKey(LL fi, LL first_part_public_key);
 
 //массив для простых чисел
-std::vector<long int> g_array_prime_numbers;
+std::vector<LL> g_array_prime_numbers;
+
+//файлы для кодирования сообщений
+std::ifstream g_file_message(std::filesystem::current_path().string() + "/message.txt");
+std::ofstream g_file_encrypt_message(std::filesystem::current_path().string() + "/encrypt_message.txt");
+std::ifstream g_file_need_encrypt_message(std::filesystem::current_path().string() + "/encrypt_message.txt");
 
 int main()
 {
@@ -37,9 +46,10 @@ int main()
     //n = q * p или first_prime * second_prime
     //fi это функция Эйлера fi = (first_prime - 1) * (second_prime - 1)
     //
-    long int first_prime, second_prime, n, fi;
+    LL first_prime, second_prime, n, fi;
+    std::vector<LL> encrypt_message_in_dec;
     std::string message_need_to_encrypt, encrypt_message = "", decrypt_message = "";
-    std::pair<long int, long int> private_key, public_key;
+    std::pair<LL, LL> private_key, public_key;
 
     //постоянно рандомные числа
     std::srand(std::time(NULL));
@@ -50,6 +60,11 @@ int main()
     //получаю рандомные простые числа из тех, которые есть в массиве
     first_prime = GetRandomPrimeNumber();
     second_prime = GetRandomPrimeNumber();
+
+    while (second_prime == first_prime)
+    {
+        second_prime = GetRandomPrimeNumber();
+    }
 
     //составляем модуль произведений и вторые части ключей
     n = first_prime * second_prime;
@@ -63,23 +78,40 @@ int main()
     public_key.first = GetFirstPartPublicKey(fi);
     private_key.first = GetFirstPartPrivateKey(fi, public_key.first);
     //вывод полученных простых чисел и числа Эйлера для проверки
-    std::cout <<"First_prime = " << first_prime << " Second_prime = "
+    std::cout << "First_prime = " << first_prime << " Second_prime = "
         << second_prime << " FI = " << fi << std::endl;
-    std::cout << "Enter your message: " << std::endl;
 
-    std::cin >> message_need_to_encrypt;
+    //считываем значения с файла
+    std::getline(g_file_message, message_need_to_encrypt);
+
+    std::cout << "Your message is: " << std::endl;
+    std::cout << message_need_to_encrypt << std::endl;
 
     std::cout << std::endl << std::endl;
 
-    EncryptMessage(message_need_to_encrypt, encrypt_message, public_key);
+    printf("Open Key is: (%lld, %lld)\n", public_key.first, public_key.second);
+    printf("Secret Key is: (%lld, %lld)\n", private_key.first, private_key.second);
 
-    printf("Open Key is: (%ld, %ld)\n", public_key.first, public_key.second);
-    printf("Secret Key is: (%ld, %ld)\n", private_key.first, private_key.second);
-
-    printf("Encrypt massage is: \n");
+    EncryptMessage(message_need_to_encrypt, encrypt_message_in_dec, public_key, encrypt_message);
+    printf("Encrypt message is: \n");
     std::cout << encrypt_message << std::endl;
 
-    DecryptMessage(encrypt_message, decrypt_message, private_key);
+    g_file_encrypt_message << encrypt_message;
+
+    g_file_need_encrypt_message >> encrypt_message;
+
+    encrypt_message_in_dec.clear();
+    size_t pos = encrypt_message.find(" ");
+    while (pos != std::string::npos)
+    {
+        encrypt_message_in_dec.push_back((LL)std::stoi(encrypt_message.substr(0, pos)));
+        encrypt_message.erase(0, pos + 1);
+        pos = encrypt_message.find(" ");
+    }
+
+
+    DecryptMessage(encrypt_message_in_dec, decrypt_message, private_key);
+    printf("Decrypt message is: \n");
     std::cout << decrypt_message << std::endl;
 }
 
@@ -94,16 +126,16 @@ void MakeArrayWithPrimeNumbersFromElevenToN()
     additional_array.resize(N + 1);
     additional_array[0] = additional_array[1] = false;
 
-    for (long int i = 2; i <= N; i++)
+    for (LL i = 2; i <= N; i++)
     {
         additional_array[i] = true;
     }
 
-    for (long int i = 2; i * i <= N; i++)
+    for (LL i = 2; i * i <= N; i++)
     {
         if (additional_array[i])
         {
-            for (long int j = i * i; j <= N; j += i)
+            for (LL j = i * i; j <= N; j += i)
             {
                 additional_array[j] = false;
             }
@@ -111,7 +143,7 @@ void MakeArrayWithPrimeNumbersFromElevenToN()
     }
     //я решил брать крайним простым числом "11" просто так 
     //(чтобы были большие значения при маленьких значениях N)
-    for (int i = 11; i <= N; i++)
+    for (int i = 3; i <= N; i++)
     {
         if (additional_array[i])
         {
@@ -120,34 +152,33 @@ void MakeArrayWithPrimeNumbersFromElevenToN()
     }
 }
 
-long int GetRandomPrimeNumber()
+LL GetRandomPrimeNumber()
 {
     return g_array_prime_numbers[std::rand() % g_array_prime_numbers.size()];
 }
 
 
-long int GetRandomPrimeInRange(long int left_border, long int right_border)
+LL GetRandomPrimeInRange(LL left_border, LL right_border)
 {
     return g_array_prime_numbers[std::rand() % g_array_prime_numbers.size()];
 }
 
-long int GetFirstPartPublicKey(long int fi)
+LL GetFirstPartPublicKey(LL fi)
 {
     //это число (e) должно быть простым, меньше значения функции Эйлера (fi),
     //а также взаимнопростым с ним (fi не должно делиться на e)
-    bool flag = true;
-    long int first_part;
-
-    while (flag)
+    for (LL first_part : g_array_prime_numbers)
     {
-        first_part = GetRandomPrimeInRange(0, fi);
-        flag = (fi % first_part == 0) || (fi < first_part);
+        if ((fi % first_part != 0) && (fi > first_part))
+        {
+            return first_part;
+        }
     }
 
-    return first_part;
+    return -1;
 }
 
-long int GetFirstPartPrivateKey(long int fi, long int first_part_public_key)
+LL GetFirstPartPrivateKey(LL fi, LL first_part_public_key)
 {
     //произведение этого числа (это число есть d) и первой части открытого ключа (e)
     //должно иметь остаток от деления (1) на число Эйлера (fi)
@@ -156,22 +187,23 @@ long int GetFirstPartPrivateKey(long int fi, long int first_part_public_key)
     while (first_part_public_key * i % fi != 1)
     {
         i++;
+        //std::cout << i << std::endl;
     }
-
+    
     return i;
 }
 
 
-long int GetEncryptNumber(long int number, std::pair<long int, long int> public_key)
+LL GetEncryptNumber(LL number, std::pair<LL, LL> public_key)
 {
     //кодирование происходит по схеме:
     //1) возводим число, которое хотим зашифровать в степень 
     //(ранее вычисленную первую часть открытого ключа) number^public_key.first
     //2) получаем остаток от деления на вторую часть открытого ключа
     //number^public_key.first % public_key.second
-    long int encryptNumber = 1, current = number - 97;
+    LL encryptNumber = 1, current = number - 97;
 
-    for (long int i = 0; i < public_key.first; i++)
+    for (LL i = 0; i < public_key.first; i++)
     {
         //так как компьютер не умеет работать с такими большими числами я решил взять логику,
         //которую подсмотрел в этом репозитории: (если не заменять его код частично моим,
@@ -186,9 +218,9 @@ long int GetEncryptNumber(long int number, std::pair<long int, long int> public_
         /* --------------------------------------------------- */
         /* --------------------------------------------------- */
         /* --------------------------------------------------- */
-        //long int encryptNumber = number;
+        //LL encryptNumber = number;
 
-        //for (long int i = 0; i < public_key.first; i++)
+        //for (LL i = 0; i < public_key.first; i++)
         //{
         //    encryptNumber *= number;
         //    encryptNumber = encryptNumber % public_key.second;
@@ -209,25 +241,26 @@ long int GetEncryptNumber(long int number, std::pair<long int, long int> public_
 
 
 
-void EncryptMessage(std::string message, std::string &encrypt_message, 
-    std::pair<long int, long int> public_key)
+void EncryptMessage(std::string message, std::vector<LL> &encrypt_message_in_dec, 
+    std::pair<LL, LL> public_key, std::string &encrypt_message)
 {
     for (char c : message)
     {// тут я получаю значение нового символа ASCII из зашифрованного числа
-        encrypt_message += char(GetEncryptNumber((long int)c, public_key));
+        encrypt_message_in_dec.push_back(GetEncryptNumber(c, public_key));
+        encrypt_message += std::to_string(encrypt_message_in_dec.back()) + " ";
     }
 }
 
-long int GetDecryptNumber(long int number, std::pair<long int, long int> private_key)
+LL GetDecryptNumber(LL number, std::pair<LL, LL> private_key)
 {
     //дешифрование проводится примерно также, как и шифрование:
     //1) возводим число в степень первой части закрытого ключа 
     // answer = (number^private_key.first)
     //2) получаем остаток от деления на вторую часть закрытого ключа 
     //answer % private_key.second
-    long int decryptNumber = 1, current = number;
+    LL decryptNumber = 1, current = number;
 
-    for (long int i = 0; i < private_key.first; i++)
+    for (LL i = 0; i < private_key.first; i++)
     {
         //так как компьютер не умеет работать с такими большими числами я решил взять логику,
         //которую подсмотрел в этом репозитории: (если не заменять его код частично моим,
@@ -236,17 +269,16 @@ long int GetDecryptNumber(long int number, std::pair<long int, long int> private
         //как я понимаю это место проблемы всего кода, но я не знаю, как 
         //правильно работать с огромными числами
         decryptNumber *= current;
-        decryptNumber = decryptNumber % private_key.second;
-
+        decryptNumber %= private_key.second;
 
         //до того как я полностью скопировал оттуда код у меня была примерно такая реализация:
         /* --------------------------------------------------- */
         /* --------------------------------------------------- */
         /* --------------------------------------------------- */
         /* --------------------------------------------------- */
-        //long int decryptNumber = number;
+        //LL decryptNumber = number;
 
-        //for (long int i = 0; i < private_key.first; i++)
+        //for (LL i = 0; i < private_key.first; i++)
         //{
         //    decryptNumber *= number;
         //    decryptNumber = decryptNumber % private_key.second;
@@ -265,11 +297,11 @@ long int GetDecryptNumber(long int number, std::pair<long int, long int> private
     return decryptNumber + 97;
 }
 
-void DecryptMessage(std::string encrypt_message, std::string& decrypt_message,
-    std::pair<long int, long int> private_key)
+void DecryptMessage(std::vector<LL>& encrypt_message_in_dec, std::string& decrypt_message,
+    std::pair<LL, LL> private_key)
 {
-    for (char c : encrypt_message)
+    for (LL c : encrypt_message_in_dec)
     {// тут я получаю значение нового символа ASCII из декодированного числа
-        decrypt_message += char(GetDecryptNumber((long int)c, private_key));
+        decrypt_message += char(GetDecryptNumber(c, private_key));
     }
 }
